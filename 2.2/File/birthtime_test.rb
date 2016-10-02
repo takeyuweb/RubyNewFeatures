4abb4e02fa7ca3c9a84e403c43b4520de8152ce5
Yuichi Takeuchi <info@takeyu-web.com>
2016-10-03 08:28:58 +0900

    File#birthtime

--- 2.2/File/birthtime_test.rb
+++ 2.2/File/birthtime_test.rb
@@ -4,7 +4,11 @@ class TestFileBirthtime < Test::Unit::TestCase
 
   def test_birthtime
     assert_instance_of(Time, File.birthtime(__FILE__))
-    p File.birthtime(__FILE__)
+  end
+
+  def test_instance_birthtime
+    file = File.open(__FILE__)
+    assert_instance_of(Time, file.birthtime)
   end
 
 end
