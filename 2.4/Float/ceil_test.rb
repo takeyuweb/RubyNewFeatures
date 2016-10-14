0f3e4b82a0d3f5b34bbe252728f109e6cfe56826
Yuichi Takeuchi <info@takeyu-web.com>
2016-10-14 20:34:22 +0900

    Float#ceil(digits), Float#floor(digits)

---
+++ 2.4/Float/ceil_test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+
+class TestFloatCeil < Test::Unit::TestCase
+
+  def test_ceil
+    assert_equal(3.2, Math::PI.ceil(1))
+    assert_equal(3.15, Math::PI.ceil(2))
+    assert_equal(3.142, Math::PI.ceil(3))
+  end
+
+  def test_floor
+    assert_equal(3.1, Math::PI.floor(1))
+    assert_equal(3.14, Math::PI.floor(2))
+    assert_equal(3.141, Math::PI.floor(3))
+  end
+
+end
