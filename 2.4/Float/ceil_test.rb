20e8b61be291e2f87a097c7e3cb80d675178b856
Yuichi Takeuchi <info@takeyu-web.com>
2016-10-15 14:55:56 +0900

    Float#truncate

--- 2.4/Float/ceil_test.rb
+++ 2.4/Float/ceil_test.rb
@@ -14,6 +14,23 @@ class TestFloatCeil < Test::Unit::TestCase
     assert_equal(3.1, Math::PI.floor(1))
     assert_equal(3.14, Math::PI.floor(2))
     assert_equal(3.141, Math::PI.floor(3))
+
+    assert_equal(-4, (-Math::PI).floor)
+    assert_equal(-3.2, (-Math::PI).floor(1))
+    assert_equal(-3.15, (-Math::PI).floor(2))
+    assert_equal(-3.142, (-Math::PI).floor(3))
+  end
+
+  def test_truncate
+    assert_equal(3, Math::PI.truncate)
+    assert_equal(3.1, Math::PI.truncate(1))
+    assert_equal(3.14, Math::PI.truncate(2))
+    assert_equal(3.141, Math::PI.truncate(3))
+
+    assert_equal(-3, (-Math::PI).truncate)
+    assert_equal(-3.1, (-Math::PI).truncate(1))
+    assert_equal(-3.14, (-Math::PI).truncate(2))
+    assert_equal(-3.141, (-Math::PI).truncate(3))
   end
 
 end
