68315a1567790ff2aaf1687df408b61195a9ae28
Yuichi Takeuchi <info@takeyu-web.com>
2016-10-15 14:34:58 +0900

    Hash#transform_values and Hash#transform_values!

---
+++ 2.4/Hash/transform_values_test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+
+class TestHashTransformValues < Test::Unit::TestCase
+
+  def test_transform_values
+    hash = {a: 1, b: 2, c: 3}
+    transformed = hash.transform_values { |v| v ** 2 }
+    assert_equal({a: 1, b: 4, c: 9}, transformed)
+  end
+
+  def test_transform_values!
+    hash = {a: 1, b: 2, c: 3}
+    hash.transform_values! { |v| v ** 2 }
+    assert_equal({a: 1, b: 4, c: 9}, hash)
+  end
+
+end
