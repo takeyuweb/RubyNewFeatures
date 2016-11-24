0243c84fd1e53960e294f01608a12d8805be9faf
Yuichi Takeuchi <info@takeyu-web.com>
2016-11-24 23:31:27 +0900

    Time/DateTime/Dateが週末か判定する #on_weekend?

---
+++ activesupport/5.0/Time/on_weekend_test.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+require 'active_support/core_ext'
+
+class TestTimeOnWeekend < Test::Unit::TestCase
+
+  def test_on_weekend?
+    assert(Time.utc(2016, 11, 13).on_weekend?) # Sunday
+    assert_false(Time.utc(2016, 11, 14).on_weekend?) # Monday
+    assert_false(Time.utc(2016, 11, 15).on_weekend?) # Tuesday
+    assert_false(Time.utc(2016, 11, 16).on_weekend?) # Wednesday
+    assert_false(Time.utc(2016, 11, 17).on_weekend?) # Thursday
+    assert_false(Time.utc(2016, 11, 18).on_weekend?) # Friday
+    assert(Time.utc(2016, 11, 19).on_weekend?) # Saturday
+  end
+
+end
