a4895ec5690219ecf29165d75722a4c9d56ca016
takeyuweb <info@takeyu-web.com>
2016-12-20 21:53:07 +0900

    テスト中にマイグレーション内容を表示しない

--- gems/activerecord/5.0/test_helper.rb
+++ gems/activerecord/5.0/test_helper.rb
@@ -1,8 +1,10 @@
 require 'test/unit'
 require 'active_record'
 
+ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.establish_connection(
     adapter: 'mysql2',
     database: 'ruby_new_feature',
-    username: 'root'
+    username: 'root',
+    password: 'root',
 )
